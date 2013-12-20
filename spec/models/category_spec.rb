80f49b649145c9a64c3b65e9835af993d5ad71f3
Helen Hood <helen.c.hood@gmail.com>
2013-12-20 15:46:13 -0500

    test and add associations between entries and categories

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,4 +3,6 @@ require 'spec_helper'
 describe Category do
   it { should validate_presence_of :name }
   it { should validate_uniqueness_of(:name).case_insensitive }
+
+  it { should have_many :entries }
 end
--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -3,4 +3,6 @@ require 'spec_helper'
 describe Entry do
   it { should validate_presence_of :title }
   it { should validate_presence_of :description }
+
+  it { should belong_to :category }
 end
