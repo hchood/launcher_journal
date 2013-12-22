e849f9a9cac8d7fdaafa086ea9c10905245684cd
Helen Hood <helen.c.hood@gmail.com>
2013-12-22 16:43:30 -0500

    Fixed typo in entries_controller

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -3,6 +3,6 @@ class EntriesController < ActionController::Base
   respond_to :html # what does this do?
 
   def index
-    @entries = Entries.all.order('created_at DESC')
+    @entries = Entry.all.order('created_at DESC')
   end
 end
