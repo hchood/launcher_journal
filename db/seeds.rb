63ac9e3d41cfe99fff17014c6f25ab5402ff373d
Helen Hood <helen.c.hood@gmail.com>
2013-12-22 17:12:27 -0500

    Created seed file and seeded database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+filename = Rails.root.join('db/data/entries_seed.csv')
+
+CSV.foreach(filename, headers: true) do |row|
+  category_hash = { name: row['category'] }
+  Category.find_or_create_by(category_hash)
+
+  user_hash = {
+    first_name: row ['first_name'],
+    last_name: row['last_name']
+  }
+  User.find_or_create_by(user_hash)
+
+  entry_hash = {
+    title: row['title'],
+    description: row['description'],
+    category_id: Category.find_by(name: row['category']).id,
+    user_id: User.find_by(first_name: row['first_name'], last_name: row['last_name']).id
+  }
+  Entry.find_or_create_by(entry_hash)
+end
