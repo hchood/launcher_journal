1517f180241d5c9df68b2ad948cbf2d83bd4fca7
Helen Hood <helen.c.hood@gmail.com>
2013-12-23 18:56:30 -0500

    Added all CRUD operations for Categories

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ LauncherJournal::Application.routes.draw do
   root to: 'entries#index'
 
   resources :entries
-  resources :categories, only: [:index, :show, :new, :create, :edit]
+  resources :categories
 end
