9e055c70d3a0bd937b4426cd4aaaf41b570623c2
Helen Hood <helen.c.hood@gmail.com>
2013-12-23 15:02:46 -0500

    Added entries#edit route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 LauncherJournal::Application.routes.draw do
   root to: 'entries#index'
 
-  resources :entries, only: [:index, :show, :new, :create]
+  resources :entries, only: [:index, :show, :new, :create, :edit]
   resources :categories, only: [:index, :show, :new, :create, :edit]
 end
