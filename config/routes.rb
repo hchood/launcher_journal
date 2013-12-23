32461507565d3609af920c821f7fe61682134b6c
Helen Hood <helen.c.hood@gmail.com>
2013-12-23 15:32:54 -0500

    Modified resources for entries to allow all CRUD operations

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 LauncherJournal::Application.routes.draw do
   root to: 'entries#index'
 
-  resources :entries, only: [:index, :show, :new, :create, :edit, :update]
+  resources :entries
   resources :categories, only: [:index, :show, :new, :create, :edit]
 end
