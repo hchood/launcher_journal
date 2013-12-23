819a77cfe302f1f0d05104ea85ab9c53b5a3ae33
Helen Hood <helen.c.hood@gmail.com>
2013-12-23 14:55:18 -0500

    Added category_id to entry_params

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -29,6 +29,6 @@ class EntriesController < ActionController::Base
   private
 
   def entry_params
-    params.require(:entry).permit(:title, :description, :user_id)
+    params.require(:entry).permit(:title, :description, :user_id, :category_id, {include_blank: true})
   end
 end
