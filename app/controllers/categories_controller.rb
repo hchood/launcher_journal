27f0592ce160039057c53bfa2f6df077a3d2a3bd
Helen Hood <helen.c.hood@gmail.com>
2013-12-23 08:48:01 -0500

    Added categories#create action & private category_params method

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -12,4 +12,21 @@ class CategoriesController < ActionController::Base
     @category = Category.new
   end
 
+  # POST /articles
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to @category, notice: 'Category was successfully created.'
+    else
+      render action: 'new'
+    end
+  end
+
+  private
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
