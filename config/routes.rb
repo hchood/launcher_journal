3a8c782a71f5a66249d90036ed59bf56e21a222b
Helen Hood <helen.c.hood@gmail.com>
2013-12-23 08:35:20 -0500

    Added categories#new route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ LauncherJournal::Application.routes.draw do
   root to: 'entries#index'
 
   resources :entries, only: [:index, :show]
-  resources :categories, only: [:index, :show]
+  resources :categories, only: [:index, :show, :new]
 end
